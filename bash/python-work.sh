acb769c7ce2482b66f0df43d513e0334a3020f0e
Duc Tran <duccoder@gmail.com>
2015-06-21 13:49:56 +0700

    install pip & ipython

---
+++ bash/python-work.sh
@@ -0,0 +1,8 @@
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+
+# python3 dev
+sudo apt-get install python3-dev python3-pip build-essential libzmq3-dev
+
+# ipython
+sudo pip install "ipython[notebook]"
